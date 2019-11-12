c5303ccc263a70c138688c39b54004ce920dd21b
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2019-11-12 17:36:03 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -28,6 +28,12 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }].join(" ").capi
 end
 
 def aoh_lookup(aoh, row, key)
+  SINGLE_ARRAY = [DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
+JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
+PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
+KATE_G = { name:  "Kate Gompert", occupation: "None" }
+BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }]
+SINGLE_ARRAY [2]
 end
 
 def aoh_update(aoh, row, key, new_value)
